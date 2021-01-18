e58d47d96c7d6948bd87420593cb47b69f9b0320
dyekv <kogaktt@gmail.com>
2021-01-18 13:54:11 +0900

    connecting firebase

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB9H93sjT6rhXQZQVcteNk_rtgN4Bodx0M",
+    authDomain: "azul-online.firebaseapp.com",
+    projectId: "azul-online",
+    storageBucket: "azul-online.appspot.com",
+    messagingSenderId: "422665620967",
+    appId: "1:422665620967:web:d43a408b8c6375233f0142",
+    measurementId: "G-STP0LQJ7W7"
+  };
