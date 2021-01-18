f57ee0a124f3d22ff090927b7f45451bb98274dc
dyekv <kogaktt@gmail.com>
2021-01-18 21:47:48 +0900

    user state

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import React,{useEffect, useState} from 'react';
 import {ChakraProvider} from '@chakra-ui/react'
 import PortraitChacker from './components/Portrait'
 import Login from './pages/Login'
+import localForage from 'localforage'
+import {User} from './interfaces'
 
 const App = () => {
   const [user,setUser] = useState('')
+
+  const getUser = async() => {
+    const user = await localForage.getItem<User>('user')
+    if(user){
+      setUser(user.name)
+    }else{
+      // go to 
+    }
+  }
+
   useEffect(()=>{
     console.log('AppのuseEffect')
-    setUser('')
+    getUser()
   },[])
   return (
     <div className="App">
       <ChakraProvider>
       <PortraitChacker>
+        {user}
         <Login/>
       </PortraitChacker>
       </ChakraProvider>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,23 @@
-import {Box,Input,Button,Image} from '@chakra-ui/react'
+import {Box,Input,Button,Image,useToast} from '@chakra-ui/react'
 import Center from '../../components/ui/Center'
+import {useState} from 'react'
 
 const Login = () => {
+    const [name,setName] = useState('')
+    const toast = useToast()
+    const onSubmit = () => {
+        if(name === ''){
+            toast({
+                status:'warning',
+                description:'名前を入力してください',
+                duration:3000,
+                isClosable:true
+            })
+            return null
+        }
+        // TODO firebaseに登録、IDを取得し、localforageに保存する
+    }
+
     return (
     <Center>
         <Box 
@@ -11,11 +27,14 @@ const Login = () => {
         >
             <Image src={process.env.PUBLIC_URL+'/logo.png'} alt='logo' maxW='400px'/>
         <Input 
+            value={name}
+            onChange={e=>setName(e.target.value)}
             placeholder="名前を入力してください"
             m='5'
         />
         <Button
             mb='5'
+            onClick={onSubmit}
         >決定</Button>
         </Box>
     </Center>
