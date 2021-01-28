5f12115c0191140bd2a82e530eca704c7f716a6c
dyekv <kogaktt@gmail.com>
2021-01-28 23:38:26 +0900

    layout toppage

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,6 +1,8 @@
 import {useHistory} from 'react-router-dom'
 import {UserContext} from '../../App'
 import {useContext} from 'react'
+import {Image} from '@chakra-ui/react'
+import Center from '../../components/ui/Center'
 
 
 const Top = () => {
@@ -8,6 +10,7 @@ const Top = () => {
     const [user,setUser] = useContext(UserContext)
     const pushStart = () => {
         if(user){
+            // ゲーム中に落ちたときは部屋に戻す
             if(user.playRoom){
                 history.push('/room'+user.playRoom)
             }else{
@@ -19,7 +22,14 @@ const Top = () => {
     }
 
     return (
-        <div onClick={pushStart}>Top page</div>
+        <div onClick={pushStart}>
+            <Center>
+                <div>
+                    <Image src={process.env.PUBLIC_URL+'/logo.png'} alt='logo' maxW='400px'/>
+                    <p style={{textAlign:'center',color:'#6495ED',fontWeight:700,fontSize:24}}>click start</p>
+                </div>
+            </Center>
+        </div>
     )
 }
 
