c6d3094ffccda8bd79ab37756fd955db944ad266
dyekv <kogaktt@gmail.com>
2021-02-25 21:49:35 +0900

    add interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,3 +3,36 @@ export interface User {
     name:string
     playRoom:string
 }
+
+export type Tile = 'moon' | 'leaf' | 'sun' | 'snow' | 'dream' | 'first'
+export type Line = Tile[]
+
+export interface Board {
+    lines:Line[]
+}
+
+export interface Work{
+    lines:Line[]
+    overs:string[]
+}
+
+export interface Player {
+    id:string
+    name:string
+    board:Board
+    work:Work
+}
+
+export type Group = Tile[]
+
+export interface Table {
+    groups:Group[]
+    center:Tile[]
+}
+
+export interface Game {
+    id:string
+    players:Player[]
+    table:Table
+    nowPlaying:string // player id
+}
